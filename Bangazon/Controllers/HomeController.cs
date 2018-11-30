749c435204c78d94a13b88e9b0a99ca741752819
Helen Chalmers <hchalmers23@gmail.com>
2018-11-30 09:28:02 -0600

    product list sorted by most recent made products

--- Bangazon/Controllers/HomeController.cs
+++ Bangazon/Controllers/HomeController.cs
@@ -5,12 +5,40 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Bangazon.Models;
+using Bangazon.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bangazon.Controllers
 {
     public class HomeController : Controller
     {
+        public readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Cohorts
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Product.OrderByDescending(x => x.DateCreated).Take(20).ToListAsync());
+        }
         
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
 
         public IActionResult Privacy()
         {
