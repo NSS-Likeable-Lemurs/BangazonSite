2c7af1870afbf93c4f1742582a593481d648aeda
Kelly Cook <kellycook20878@gmail.com>
2018-11-30 13:48:16 -0600

    can view list of products as well

---
+++ Bangazon/Models/ProductViewModels/ProductTypesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Bangazon.Models;
+using Bangazon.Data;
+
+namespace Bangazon.Models.ProductViewModels
+{
+    public class ProductTypesViewModel
+    {
+        public Product Product { get; set; }
+        public ProductType ProductType { get; set; }
+        public List<GroupedProducts> GroupedProducts { get; set; }
+    }
+}
